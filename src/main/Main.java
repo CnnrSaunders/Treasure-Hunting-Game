02dbf08b7580b07b8878cf2a442e4bc6d3901e5a
Connor Saunders <csau0007@student.monash.edu>
2025-03-08 00:23:15 +1100

    developed the thread that the main game loop will run in.

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,6 +15,8 @@ public class Main {
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
 
+        window.pack();
+
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
