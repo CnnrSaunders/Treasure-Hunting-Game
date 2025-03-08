118c78c575a90aa059805d420e417b0f3fae014f
Connor Saunders <csau0007@student.monash.edu>
2025-03-08 20:35:58 +1100

    8/03/2025 20:00 -implemented collision against tiles that are unwalkable.

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -24,6 +24,9 @@ public class Player extends Entity{
         screenX = gp.screenWidth/2 - (gp.tileSize/2);
         screenY = gp.screenHeight/2 - (gp.tileSize/2);
 
+        solidArea = new Rectangle(8,16,32,32);
+
+
         setDefaultValues();
         getPlayerImage();
     }
@@ -60,19 +63,30 @@ public class Player extends Entity{
 
             if (keyH.upPressed == true){
                 direction = "up";
-                worldY -= speed;
             }
             else if (keyH.downPressed == true){
                 direction = "down";
-                worldY += speed;
             }
             else if (keyH.leftPressed == true){
                 direction = "left";
-                worldX -= speed;
             }
             else if (keyH.rightPressed == true){
                 direction = "right";
-                worldX += speed;
+            }
+
+            //check collison
+            collisionOn = false;
+            gp.cChecker.checkTile(this);
+
+            // if collision is false, player is able to move.
+            if (collisionOn == false){
+                switch(direction) {
+                    case "up": worldY -= speed; break;
+                    case "down": worldY += speed; break;
+                    case "left": worldX -= speed; break;
+                    case "right": worldX += speed; break;
+
+                }
             }
 
             spriteCounter++;
