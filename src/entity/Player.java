de5c321b30e1882dd5491ff2e1341a5496e17f62
Connor Saunders <csau0007@student.monash.edu>
2025-03-10 00:16:02 +1100

    10/03/2025 00:15 -updated player sprite to always reset to spriteNum 1 when standing still

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -16,6 +16,8 @@ public class Player extends Entity{
     public final int screenX;
     public final int screenY;
 
+    int standCounter = 0;
+
     public Player(GamePanel gp, KeyHandler keyH){
 
         this.gp = gp;
@@ -100,6 +102,14 @@ public class Player extends Entity{
                 spriteCounter = 0;
             }
 
+        }
+        else {
+            standCounter++;
+
+            if (standCounter == 20){
+                spriteNum = 1;
+                standCounter = 0;
+            }
         }
 
     }
