b9743fb2a4f35158ef6c8712dc064d03022db6ab
Connor Saunders <csau0007@student.monash.edu>
2025-03-13 17:00:58 +1100

    13/03/2025 17:00 - added sound effects to item pickup / door unlock - added boots to increase speed - added main game background music.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -22,18 +22,20 @@ public class GamePanel extends JPanel implements Runnable {
     //WORLD SETTINGS
     public final int maxWorldCol = 50;
     public final int maxWorldRow = 50;
-    public final int worldWidth = tileSize * maxWorldCol;
-    public final int worldHeight = tileSize * maxScreenRow;
 
     //FPS
     int FPS = 60;
 
+    //system
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
-    Thread gameThread;
+    Sound sound = new Sound();
     public CollisionChecker cChecker = new CollisionChecker(this);
     public AssetSetter aSetter = new AssetSetter(this);
+    Thread gameThread;
 
+
+    //Entity and objects
     public Player player = new Player(this, keyH);
     public SuperObject obj[] = new SuperObject[10];
 
@@ -51,6 +53,8 @@ public class GamePanel extends JPanel implements Runnable {
     public void setupGame(){
 
         aSetter.setObject();
+        // play theme song
+        playMusic(0);
     }
 
     public void startGameThread(){
@@ -105,5 +109,17 @@ public class GamePanel extends JPanel implements Runnable {
         player.draw(g2);
 
         g2.dispose();
+    }
+    public void playMusic(int i){
+        sound.setFile(i);
+        sound.play();
+        sound.loop();
+    }
+    public void stopMusic(){
+        sound.stop();
+    }
+    public void playSE(int i){
+        sound.setFile(i);
+        sound.play();
     }
 }
