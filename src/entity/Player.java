fcd5bf0d4a833bbdbb9f1ae566b05ee4c8a92d2e
Connor Saunders <csau0007@student.monash.edu>
2025-03-13 17:52:07 +1100

    13/03/2025 17:00 - added UI and the game finishing when after you find the treasure!!

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -16,7 +16,7 @@ public class Player extends Entity{
     public final int screenX;
     public final int screenY;
 
-    int hasKey = 0;
+    public int hasKey = 0;
 
     int standCounter = 0;
 
@@ -134,18 +134,29 @@ public class Player extends Entity{
                     gp.playSE(1);
                     hasKey++;
                     gp.obj[i] = null;
+                    gp.ui.showMessage("You got a key!");
                     break;
                 case "Door":
                     if (hasKey > 0){
                         gp.playSE(3);
                         gp.obj[i] = null;
                         hasKey --;
+                        gp.ui.showMessage("You opened a door!");
+                    }
+                    else{
+                        gp.ui.showMessage("The door is locked...");
                     }
                     break;
                 case "Boots":
                     gp.playSE(2);
                     speed += 2;
                     gp.obj[i] = null;
+                    gp.ui.showMessage("Speed UP!");
+                    break;
+                case "Chest":
+                    gp.ui.gameFinished = true;
+                    gp.stopMusic();
+                    gp.playSE(4);
                     break;
 
             }
--- src/main/Sound.java
+++ src/main/Sound.java
@@ -13,8 +13,9 @@ public class Sound {
     public Sound(){
         soundURL[0] = getClass().getResource("/sound/BlueBoyAdventure.wav");
         soundURL[1] = getClass().getResource("/sound/coin.wav");
-        soundURL[2] = getClass().getResource("/sound/unlock.wav");
-        soundURL[3] = getClass().getResource("/sound/fanfare.wav");
+        soundURL[2] = getClass().getResource("/sound/powerup.wav");
+        soundURL[3] = getClass().getResource("/sound/unlock.wav");
+        soundURL[4] = getClass().getResource("/sound/fanfare.wav");
 
     }
 
